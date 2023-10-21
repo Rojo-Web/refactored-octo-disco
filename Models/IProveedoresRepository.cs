238da2d8ef112f3c9c1bf7b0d0596e32bf0f67a8
Santiago Figueroa Andrade <santifa2004@gmail.com>
2023-10-21 01:15:54 -0500

    ProveedorModel y IProveedorRepository

---
+++ Models/IProveedoresRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface IProveedoresRepository
+    {
+
+        void add(ProveedoresModel proveedorModel);
+        void edit(ProveedoresModel proveedorModel);
+        void delete(int id);
+        IEnumerable<ProveedoresModel> GetAll();
+        IEnumerable<ProveedoresModel> GetByValue(string value);
+    }
+}
---
+++ Models/ProveedoresModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProveedoresModel
+    {
+        //Validadores
+        [DisplayName("Proveedor Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Proveedor Name")]
+        //Validadores
+        [Required(ErrorMessage = "Proveedor name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Proveedor name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Descripcion")]
+        [Required(ErrorMessage = "Proveedor descripcion is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Proveedor descripcion must be between 3 and 200 characters")]
+        public string Descripcion { get; set; }
+    }
+}
